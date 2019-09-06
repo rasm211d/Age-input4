ea53e03ca2b66db602a0d5de1efb6420c7e1ca4d
Rasmus Jannerup <54896192+rasm211d@users.noreply.github.com>
2019-09-06 13:52:16 +0200

    Initial commit

---
+++ src/Hello_world.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class MyClass{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Hello");
+        System.out.println("Please enter your age: ");
+        int input = s.nextInt();
+        int age = input;
+        
+    }
+}
